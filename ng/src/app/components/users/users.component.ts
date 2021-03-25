4ed6367cceccc9a8ee0a1c209308e81a224ede1e
Calin Armenean <calin.armenean@gmail.com>
2021-03-25 19:12:09 -0400

    Added bootstrap

--- ng/src/app/components/users/users.component.ts
+++ ng/src/app/components/users/users.component.ts
@@ -22,27 +22,28 @@ export class UsersComponent implements OnInit {
   filteredOptions: Observable<string[]>;
 
   ngOnInit(): void {
-
     this.api.getComments().subscribe((data: any) => {
       this.comments = data;
       this.users = [...new Set(data.map(x => x.userId))];
+      this.users.sort(function (a, b) {
+        if (a < b) { return -1; }
+        if (a > b) { return 1; }
+        return 0;
+      })
       this.filteredOptions = this.myControl.valueChanges
         .pipe(
           startWith(''),
           map(value => this._filter(value))
         );
     })
-
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-
     return this.users.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   filterUser(user) {
-    console.log(user);
     this.commentsInput = [...this.comments.filter(x => x.userId == user)];
   }
 }
